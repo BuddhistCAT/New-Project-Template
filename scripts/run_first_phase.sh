842683e1e8d871fa9d9e5037c13fa3a68e2bc9f5
Mikko Kotila <upstairs@Mikkos-Mac-mini.local>
2022-03-16 21:33:43 +0200

    cleaned up package

--- scripts/run_first_phase.sh
+++ scripts/run_first_phase.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source .languages
+
 # remove empty lines
 cat root/001.txt | grep -v -e "^$" | grep -v -e "^ " | grep -v -e "^  " > temp.txt
 mv temp.txt root/001_v2.txt
@@ -10,14 +12,16 @@ python3 scripts/sentences_to_transifex.py 001_v2
 # create directory structure folder
 mkdir stats
 mkdir wip
-mkdir wip/en
-mkdir wip/bo
+mkdir wip/$SOURCE_LANGUAGE
+mkdir wip/$TARGET_LANGUAGE
 
-# add english file
-touch wip/en/001.po
+# add add language files
+touch wip/$TARGET_LANGUAGE/001.po
+mv root/001_v2.po wip/$SOURCE_LANGUAGE/001.po
 
-# clean up
-mv root/001_v2.po wip/bo/001.po
+# create transifex config file
+cat transifex.yml | sed s'/_source_language_/'"$SOURCE_LANGUAGE"'/'g > transifex_temp.yml
+mv transifex_temp.yml transifex.yml
 
 # word_segment statistics
 python3 scripts/token_statistics.py
