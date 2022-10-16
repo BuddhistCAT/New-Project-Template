a2ad356f706448ee6b6d7c928d54bf11e476c71e
Mikko Kotila <mailme@mikkokotila.com>
2022-10-16 13:27:18 +0300

    Update export_to_html.py

--- scripts/export_to_html.py
+++ scripts/export_to_html.py
@@ -8,8 +8,6 @@ header = '''<!DOCTYPE html>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Tibetan&display=swap" rel="stylesheet">
-    
-    
 </head>
 <body>'''
 
