84a30c0201cb06ddd2e4a799f1f328bad2cdf9db
Mikko Kotila <mailme@mikkokotila.com>
2022-10-16 13:44:30 +0300

    Update export_to_csv.py

--- scripts/export_to_csv.py
+++ scripts/export_to_csv.py
@@ -1,7 +1,11 @@
-f = open('exports/en.csv', 'w')
+import sys
+
+target_language = sys.argv[0]
+
+f = open('exports/' + target_language + '.csv', 'w')
 
 import polib
-po = polib.pofile('wip/en/001.po')
+po = polib.pofile('wip/' + target_language + '/001.po')
 
 for entry in po:
 
