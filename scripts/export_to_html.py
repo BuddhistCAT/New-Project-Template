9756c174f5c0d019c1a653a9bde2aa84aa46cae8
Mikko Kotila <mailme@mikkokotila.com>
2022-10-16 13:07:47 +0300

    added export to html

---
+++ scripts/export_to_html.py
@@ -0,0 +1,33 @@
+header = '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tibetan Reader</title>
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Tibetan&display=swap" rel="stylesheet">
+    
+    
+</head>
+<body>'''
+
+tibetan = '<p class="ml-7 mb-1 mt-12 tracking-wide text-3xl text-gray-900 dark:text-white" style="font-family: Noto Serif Tibetan, serif;">'
+translation = '<p class="ml-7 text-2xl text-gray-900 dark:text-white" style="font-family: Noto Serif, serif;">'
+footer = '<script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script></body></html>'
+
+f = open('index.html', 'w')
+
+import polib
+po = polib.pofile('wip/en/001.po')
+
+f.write(header)
+out = []
+for entry in po:
+
+    f.write(tibetan + entry.msgid.replace(' ' ,'').replace('␣', '').replace(' ', ' '))
+    f.write(translation + entry.msgstr)
+
+f.write(footer)
+f.close()
